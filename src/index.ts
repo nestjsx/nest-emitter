1769c149dad12af249c20be5bed342ff80ac2791
Shady Khalifa <shekohex@gmail.com>
2018-09-01 11:02:29 +0200

    fixed the types error, and it works now :tada:

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { NestEmitterModule } from './nest-emitter.module';
+export { StrictEventEmitter, StrictBroadcast } from './strong-emitter';
+export { InjectEventEmitter } from './event-emitter.decorator';
--- src/nest-emitter.module.ts
+++ src/nest-emitter.module.ts
@@ -1,15 +1,11 @@
 import { Module, DynamicModule, Global, Provider } from '@nestjs/common';
 import { EVENT_EMITTER_TOKEN } from './constants';
-import { StrictEventEmitter } from './strong-emitter';
+type NestEmitter = NodeJS.Events;
 @Global()
 @Module({})
 export class NestEmitterModule {
-  public static forRoot<TEventRecord, TEmitRecord = TEventRecord>(
-    emitter: NodeJS.Events,
-  ): DynamicModule {
-    // FIXME: fix the types errors here !
-    const instance: StrictEventEmitter<NodeJS.Events, TEventRecord, TEmitRecord> = emitter;
-    const providers: Provider[] = [{ provide: EVENT_EMITTER_TOKEN, useValue: instance }];
+  public static forRoot(emitter: NestEmitter): DynamicModule {
+    const providers: Provider[] = [{ provide: EVENT_EMITTER_TOKEN, useValue: emitter }];
     return { module: NestEmitterModule, components: providers, exports: providers };
   }
 }
