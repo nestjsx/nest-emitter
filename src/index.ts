1bda5115326798b7b4f78ea74cb65a78324943b5
Shady Khalifa <shekohex@gmail.com>
2018-12-06 23:04:10 +0200

    feat: export Token for testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { NestEmitterModule } from './nest-emitter.module';
 export { StrictEventEmitter, StrictBroadcast } from './strong-emitter';
 export { InjectEventEmitter } from './event-emitter.decorator';
+export { EVENT_EMITTER_TOKEN } from './constants';
